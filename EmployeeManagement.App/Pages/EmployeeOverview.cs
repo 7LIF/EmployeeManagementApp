fe4a91d8b68b60738459282323647a3f64ae95f9
MARIANA_ALBERTO.prog/ <mariana_alberto@live.com.pt>
2023-07-18 15:39:37 +0100

    Add template

--- EmployeeManagement.App/Pages/EmployeeOverview.cs
+++ EmployeeManagement.App/Pages/EmployeeOverview.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using EmployeeManagement.App.Components;
 
+
+
 namespace EmployeeManagement.App.Pages
 {
     public partial class EmployeeOverview
@@ -19,7 +21,15 @@ namespace EmployeeManagement.App.Pages
         public IEmployeeDataService EmployeeDataService { get; set; }
 
 
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+
         protected AddEmployeeDialog AddEmployeeDialog { get; set; }
+
+
+        [Parameter]
+        public EventCallback<bool> OnClickEventCallback { get; set; }
 
 
         protected async override Task OnInitializedAsync()
@@ -39,6 +49,20 @@ namespace EmployeeManagement.App.Pages
             Employees = (await EmployeeDataService.GetAllEmployees()).ToList();
             StateHasChanged();
         }
+
+
+        protected void NavigateToOverview()
+        {
+            NavigationManager.NavigateTo("/employeeoverview", forceLoad: true);
+        }
+
+
+        protected async Task DeleteEmployee(int employeeId)
+        {
+            await EmployeeDataService.DeleteEmployee(employeeId);
+            NavigateToOverview();
+        }
+
 
     }
 }
